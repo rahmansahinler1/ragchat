1d72cd6e5add87925ef676e744ec4da79bfd3eb0
Ibrahim Yasin Goktas <ibrahimyasing@gmail.com>
2024-10-02 02:55:18 +0300

    feat:completed home page and recolored sign up/in pages

--- static/js/main.js
+++ static/js/main.js
@@ -82,6 +82,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const passwordInput = document.getElementById("password");
             if (passwordInput.type === "password") {
               passwordInput.type = "text";
+              
             } else {
               passwordInput.type = "password";
             }
