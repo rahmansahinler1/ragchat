99b6a53eb129ac5b67b1d2e9f88b0b68c153b259
albatrosfirst <nazm.albatros@gmail.com>
2024-12-14 11:38:05 +0300

    feat: pptx and xlsx is implemented

--- app/functions/reading_functions.py
+++ app/functions/reading_functions.py
@@ -6,9 +6,13 @@ import spacy
 import pymupdf4llm
 from pathlib import Path
 from langchain_text_splitters import MarkdownHeaderTextSplitter
-from docling.document_converter import DocumentConverter, WordFormatOption
 from docling.datamodel.base_models import InputFormat
 from docling.pipeline.simple_pipeline import SimplePipeline
+from docling.document_converter import (
+    DocumentConverter,
+    WordFormatOption,
+    PowerpointFormatOption,
+)
 
 
 class ReadingFunctions:
@@ -35,11 +39,10 @@ class ReadingFunctions:
             self.headers_to_split_on, strip_headers=False, return_each_line=True
         )
         self.converter = DocumentConverter(
-            allowed_formats=[
-                InputFormat.DOCX,
-            ],
+            allowed_formats=[InputFormat.DOCX, InputFormat.PPTX, InputFormat.XLSX],
             format_options={
-                InputFormat.DOCX: WordFormatOption(pipeline_cls=SimplePipeline)
+                InputFormat.DOCX: WordFormatOption(pipeline_cls=SimplePipeline),
+                InputFormat.PPTX: PowerpointFormatOption(pipeline_cls=SimplePipeline),
             },
         )
 
@@ -56,6 +59,10 @@ class ReadingFunctions:
                 return self._process_pdf(file_bytes=file_bytes)
             elif file_type == "docx":
                 return self._process_docx(file_bytes=file_bytes)
+            elif file_type == "pptx":
+                return self._process_pptx(file_bytes=file_bytes)
+            elif file_type == "xlsx":
+                return self._process_xlsx(file_bytes=file_bytes)
             elif file_type in ["txt", "rtf"]:
                 return self._process_txt(file_bytes=file_bytes)
             else:
@@ -188,6 +195,148 @@ class ReadingFunctions:
                     docx_data["page_number"].append(current_page)
                     current_length += len(split.page_content)
         return docx_data
+
+    def _process_pptx(self, file_bytes: bytes):
+        pptx_data = {
+            "sentences": [],
+            "page_number": [],
+            "is_header": [],
+            "is_table": [],
+        }
+        current_length = 0
+        chars_per_page = 500
+        current_page = 1
+
+        pptx_file = io.BytesIO(file_bytes)
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".pptx") as temp_file:
+            temp_file.write(pptx_file.getvalue())
+            pptx_path = Path(temp_file.name)
+            md_text = self.converter.convert(pptx_path).document.export_to_markdown()
+            splits = self.markdown_splitter.split_text(md_text)
+            for split in splits:
+                if current_length + len(split.page_content) > chars_per_page:
+                    current_page += 1
+                    current_length = 0
+
+                if (
+                    not len(split.page_content) > 5
+                    or re.match(r"^[^\w]*$", split.page_content)
+                    or split.page_content[:4] == "<!--"
+                ):
+                    continue
+                elif (
+                    split.metadata and split.page_content[0] == "#"
+                ):  # Header detection
+                    pptx_data["sentences"].append(split.page_content)
+                    pptx_data["is_header"].append(True)
+                    pptx_data["is_table"].append(False)
+                    pptx_data["page_number"].append(current_page)
+                    current_length += len(split.page_content)
+                elif (
+                    split.page_content[0] == "*"
+                    and split.page_content[-1] == "*"
+                    and (
+                        re.match(
+                            r"(\*{2,})(\d+(?:\.\d+)*)\s*(\*{2,})?(.*)$",
+                            split.page_content,
+                        )
+                        or re.match(
+                            r"(\*{1,3})?([A-Z][a-zA-Z\s\-]+)(\*{1,3})?$",
+                            split.page_content,
+                        )
+                    )
+                ):  # Sub-Header and Header variant detection
+                    pptx_data["sentences"].append(split.page_content)
+                    pptx_data["is_header"].append(True)
+                    pptx_data["is_table"].append(False)
+                    pptx_data["page_number"].append(current_page)
+                    current_length += len(split.page_content)
+                elif (
+                    split.page_content[0] == "|" and split.page_content[-1] == "|"
+                ):  # Table detection
+                    pptx_data["sentences"].append(split.page_content)
+                    pptx_data["is_header"].append(False)
+                    pptx_data["is_table"].append(True)
+                    pptx_data["page_number"].append(current_page)
+                    current_length += len(split.page_content)
+                else:
+                    pptx_data["sentences"].append(split.page_content)
+                    pptx_data["is_header"].append(False)
+                    pptx_data["is_table"].append(False)
+                    pptx_data["page_number"].append(current_page)
+                    current_length += len(split.page_content)
+        return pptx_data
+
+    def _process_xlsx(self, file_bytes: bytes):
+        xlsx_data = {
+            "sentences": [],
+            "page_number": [],
+            "is_header": [],
+            "is_table": [],
+        }
+        current_length = 0
+        chars_per_page = 2000
+        current_page = 1
+
+        xlsx_file = io.BytesIO(file_bytes)
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".xlsx") as temp_file:
+            temp_file.write(xlsx_file.getvalue())
+            xlsx_path = Path(temp_file.name)
+            md_text = self.converter.convert(xlsx_path).document.export_to_markdown()
+            splits = self.markdown_splitter.split_text(md_text)
+            for split in splits:
+                if current_length + len(split.page_content) > chars_per_page:
+                    current_page += 1
+                    current_length = 0
+
+                if (
+                    not len(split.page_content) > 5
+                    or re.match(r"^[^\w]*$", split.page_content)
+                    or split.page_content[:4] == "<!--"
+                ):
+                    continue
+                elif (
+                    split.metadata and split.page_content[0] == "#"
+                ):  # Header detection
+                    xlsx_data["sentences"].append(split.page_content)
+                    xlsx_data["is_header"].append(True)
+                    xlsx_data["is_table"].append(False)
+                    xlsx_data["page_number"].append(current_page)
+                    current_length += len(split.page_content)
+                elif (
+                    split.page_content[0] == "*"
+                    and split.page_content[-1] == "*"
+                    and (
+                        re.match(
+                            r"(\*{2,})(\d+(?:\.\d+)*)\s*(\*{2,})?(.*)$",
+                            split.page_content,
+                        )
+                        or re.match(
+                            r"(\*{1,3})?([A-Z][a-zA-Z\s\-]+)(\*{1,3})?$",
+                            split.page_content,
+                        )
+                    )
+                ):  # Sub-Header and Header variant detection
+                    xlsx_data["sentences"].append(split.page_content)
+                    xlsx_data["is_header"].append(True)
+                    xlsx_data["is_table"].append(False)
+                    xlsx_data["page_number"].append(current_page)
+                    current_length += len(split.page_content)
+                elif (
+                    split.page_content[0] == "|" and split.page_content[-1] == "|"
+                ):  # Table detection
+                    xlsx_data["sentences"].append(split.page_content)
+                    xlsx_data["is_header"].append(False)
+                    xlsx_data["is_table"].append(True)
+                    xlsx_data["page_number"].append(current_page)
+                    current_length += len(split.page_content)
+                else:
+                    xlsx_data["sentences"].append(split.page_content)
+                    xlsx_data["is_header"].append(False)
+                    xlsx_data["is_table"].append(False)
+                    xlsx_data["page_number"].append(current_page)
+                    current_length += len(split.page_content)
+        return xlsx_data
 
     def _process_txt(self, file_bytes: bytes):
         text_data = {
