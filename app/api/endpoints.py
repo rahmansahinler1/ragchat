e9cd2c6eac28931fb521a9f02b0dccf7455c1dbf
karstaag <rahmansahinler1@gmail.com>
2025-01-12 12:00:28 +0300

    fix: prod uri

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 # environment variables
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI_DEV")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
 
 
 # request functions
